706392a7765ade4f77834120e77557ef6ca9d61e
Jesse Theisen <theiseja@gmail.com>
2020-05-08 14:52:06 -0500

    Starting work on navbar

--- app/src/Components/Navbar.js
+++ app/src/Components/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link, Router } from 'react-router-dom'
 
 const Navbar = () => {
     return(
+        <Router>
         <nav className="nav-wrapper">
             <div className="container">
                 <Link to="/" className="brand-logo">Shopping</Link>
-
                 <ul className="right-nav">
                     <li><Link to="/">Shop</Link></li>
                     <li><Link to="/cart">My cart</Link></li>
@@ -14,6 +14,7 @@ const Navbar = () => {
                 </ul>
             </div>
         </nav>
+        </Router>
     )
 }
 
--- app/src/index.js
+++ app/src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import cartReducer from './Components/reducers/cartReducer';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(cartReducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
